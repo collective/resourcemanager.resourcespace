55023379570d2e254339fb1e1807187281155849
Chrissy Wainwright <chrissy@sixfeetup.com>
2020-01-29 10:50:36 -0600

    use upload_file_by_url to upload
    curently will only upload when editing an existing image, but it doesn't
    use the new image. Need to save first, then upload

--- src/resourcemanager/resourcespace/events.py
+++ src/resourcemanager/resourcespace/events.py
@@ -6,6 +6,29 @@ from resourcemanager.resourcespace.search import ResourceSpaceSearch
 
 def upload_image(obj, event):
     """When a Plone image is modified, sync changes to RS
+       Use the image's url to upload
+    """
+    # if image has a resource id, update that resource (if it still exists)
+    # otherwise, add the resource
+    rs_search = ResourceSpaceSearch(obj, obj.REQUEST)
+    # param7 will be for metadata
+    query = '&function=create_resource&param1=1&param2=0'
+    resource_id = rs_search.query_resourcespace(query)
+
+    rs_search.query_resourcespace(
+        '&function=upload_file_by_url&param1={0}&param5={1}'.format(
+            resource_id, 'http://0c1d467e.ngrok.io' + '/'.join(obj.getPhysicalPath())
+        ))
+    # put into test collection for now
+    rs_search.query_resourcespace(
+        '&function=add_resource_to_collection&param1={}&param2=1'.format(
+            resource_id
+        ))
+
+
+def upload_image_file(obj, event):
+    """When a Plone image is modified, sync changes to RS
+       This one tries to upload the image as a file
     """
     # if image has a resource id, update that resource (if it still exists)
     # otherwise, add the resource
@@ -29,6 +52,6 @@ def upload_image(obj, event):
     os.system('rm -rf {}'.format(exp_path))
     # put into test collection for now #3151
     rs_search.query_resourcespace(
-        '&function=add_resource_to_collection&param1={}&param2=6'.format(
+        '&function=add_resource_to_collection&param1={}&param2=1'.format(
             resource_id
         ))
